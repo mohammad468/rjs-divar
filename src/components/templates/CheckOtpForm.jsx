f7469d77c19bcad55ef6cf707da2fb720410e77d
mohammad468 <mohammad.mohseni468@gmail.com>
2024-12-14 13:15:44 +0330

    complete project with tailwindcss

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -4,7 +4,7 @@ import { checkOtp } from "services/auth";
 import { getProfile } from "src/services/user";
 import { setCookie } from "src/utils/cookie";
 
-import styles from "./CheckOtpForm.module.css";
+const buttonClasses = "w-full px-2 py-2 mt-5 rounded-lg border border-red-800 transition-all ease-in-out duration-300";
 
 function CheckOtpForm({ mobile, code, setCode, setStep, setError }) {
   const navigate = useNavigate();
@@ -20,24 +20,20 @@ function CheckOtpForm({ mobile, code, setCode, setStep, setError }) {
       navigate("/");
       refetch();
     }
-    if (error) setError(error.response.data.message);
+    if (error) {
+      setError(error.response.data.message);
+    }
   };
 
   return (
-    <form onSubmit={submitHandler} className={styles.form}>
-      <p>تایید کد پیامک شده</p>
-      <span>کد پیامک شده به شماره {mobile} را وارد کنید</span>
+    <form onSubmit={submitHandler} className="max-w-lg m-auto flex flex-col justify-center border-2 border-gray-200 rounded-lg p-8 mt-24">
+      <p className="text-lg font-bold mb-5">تایید کد پیامک شده</p>
+      <span className="text-gray-500 mb-5">کد پیامک شده به شماره {mobile} را وارد کنید</span>
       <label htmlFor="input">کد تایید را وارد کنید</label>
-      <input
-        type="text"
-        id="input"
-        placeholder="کد تایید"
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-      />
+      <input type="text" className="form-input mt-0 p-1 border-2 rounded-md border-gray-200 focus:border-gray-400 focus:ring-white" id="input" placeholder="کد تایید" value={code} onChange={(e) => setCode(e.target.value)} />
       <div>
-        <button type="submit">ورود</button>
-        <button className={styles.backButton} onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+        <button className={`${buttonClasses} md:w-28 bg-red-800 text-white ml-1`} type="submit">ورود</button>
+        <button className={`${buttonClasses} md:w-40 text-red-800 bg-white mr-1 hover:bg-red-800 hover:text-white`} onClick={() => setStep(1)}>تغییر شماره موبایل</button>
       </div>
     </form>
   );
--- src/components/templates/SendOtpFrom.jsx
+++ src/components/templates/SendOtpFrom.jsx
@@ -1,30 +1,65 @@
+import { useEffect, useState } from "react";
 import { sendOtp } from "services/auth";
-import styles from "./SendOtpForm.module.css";
+
+const description =
+  "برای استفاده از امکانات دیوار، لطفا شماره موبایل خود را وارد کنید. تایید به این شماره پیامک خواهد شد.";
 
 function SendOtpFrom({ setStep, mobile, setMobile, setError }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [validNumber, setValidNumber] = useState(true);
+  const [invalidDescription, setInvalidDescription] = useState("");
   const submitHandler = async (e) => {
     e.preventDefault();
-    if (mobile && mobile.length !== 11) return;
+    if (mobile && mobile.length !== 11) {
+      setValidNumber(false);
+      setInvalidDescription("شماره موبایل نامعتبر میباشد");
+      return;
+    }
+    if(mobile && mobile.length === 11) {
+      setValidNumber(true);
+      setInvalidDescription("");
+    }
+    setValidNumber(true);
+    setIsLoading(true);
     const { response, error } = await sendOtp(mobile);
+    setIsLoading(false);
     if (response) setStep(2);
     if (error) setError(error.response.data.message);
   };
+
+  useEffect(() => {
+    if(mobile && mobile.length === 11) {
+      setValidNumber(true);
+      setInvalidDescription("");
+    }
+  }, [mobile]);
+
   return (
     <div>
-      <form onSubmit={submitHandler} className={styles.form}>
-        <p>ورود به حساب کاربری</p>
-        <span>
-          برای استفاده از امکانات دیوار، لطفا شماره موبایل خود را وارد کنید. کد تایید به این شماره
-          پیامک خواهد شد.
-        </span>
+      <form
+        onSubmit={submitHandler}
+        className="max-w-lg m-auto flex flex-col justify-center border-2 border-gray-200 rounded-lg p-8 mt-24"
+      >
+        <p className="text-lg font-bold mb-5">ورود به حساب کاربری</p>
+        <span className="text-gray-500 mb-5">{description}</span>
         <label htmlFor="input">شماره موبایل خود را وارد کنید</label>
         <input
           type="input"
+          className="form-input mt-0 p-1 border-2 rounded-md border-gray-200 focus:border-gray-400 focus:ring-white"
           placeholder="شماره موبایل"
           value={mobile}
           onChange={(e) => setMobile(e.target.value)}
         />
-        <button type="submit">ارسال کد تایید</button>
+        {!validNumber && <span className="text-red-600 text-sm font-extralight">{invalidDescription}</span>}
+        {isLoading ? (
+          <button className="w-32 px-2 py-2 mt-5 bg-red-400 text-white rounded-lg" disabled>
+            در حال ارسال...
+          </button>
+        ) : (
+          <button className="w-28 px-2 py-2 mt-5 bg-red-800 text-white rounded-lg" type="submit">
+            ارسال کد تایید
+          </button>
+        )}
       </form>
     </div>
   );
