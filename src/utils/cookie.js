3a89a7419f9f06566a812b7bb1b376374ab8bb29
mohammad468 <mohammad.mohseni468@gmail.com>
2025-03-24 16:02:02 +0330

    debug create refreshToken empty in cookies

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 
 const setCookie = (tokens) => {
   document.cookie = `accessToken=${tokens.accessToken}; max-age=${1 * 24 * 60 * 60}`;
-  document.cookie = `refreshToken=${tokens.refreshToken}; max-age=${30 * 24 * 60 * 60}`;
+  // document.cookie = `refreshToken=${tokens.refreshToken}; max-age=${30 * 24 * 60 * 60}`;
 };
 
 const getCookie = (cookieName) => {
