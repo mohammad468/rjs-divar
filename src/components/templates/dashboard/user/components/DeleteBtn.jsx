835a879a523525814e8d9b99de640787f247ec23
mohammad468 <mohammad.mohseni468@gmail.com>
2025-03-28 15:35:34 +0330

    add refresh user list when onSuccess delete user

--- src/components/templates/dashboard/user/components/DeleteBtn.jsx
+++ src/components/templates/dashboard/user/components/DeleteBtn.jsx
@@ -1,13 +1,17 @@
 import { RiDeleteBin6Line } from "react-icons/ri";
 import toast, { Toaster } from "react-hot-toast";
-import { useMutation } from "@tanstack/react-query";
-import { deleteUser } from "src/services/user";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteUser, getUsers } from "src/services/user";
 
 const classBtn = "p-2 text-red-500 bg-red-100 rounded hover:bg-red-200 transition duration-300";
 
 function DeleteBtn({ userId }) {
+  const { refetch } = useQuery(["users"], getUsers);
   const { mutate } = useMutation((id) => deleteUser(id), {
-    onSuccess: () => toast.success("حذف با موفقیت انجام شد"),
+    onSuccess: () => {
+      refetch();
+      toast.success("حذف با موفقیت انجام شد");
+    },
     onError: () => toast.error("خطا در حذف کاربر"),
   });
 
